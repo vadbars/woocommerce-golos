b3f96b91bc6fe5affc23ea3680fa21b6d4ab0010
Vadim Barsukov <vadbars@gmail.com>
2017-11-25 15:41:05 +0500

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -1,36 +1,36 @@
 <?php
 /**
  * Plugin Name: WooCommerce Steem/Golos
- * Plugin URI: https://github.com/recrypto/woocommerce-steem
- * Description: Accept Steem payments directly to your shop (Currencies: STEEM, SBD).
- * Version: 1.0.5
- * Author: ReCrypto
+ * Plugin URI: https://github.com/vadbars/woocommerce-golos
+ * Description: Accept Golos payments directly to your shop (Currencies: GOLOS, GBG).
+ * Version: 0.0.1
+ * Author: Vadbars / ReCrypto
  * Author URI: https://steemit.com/@recrypto
  * Requires at least: 4.1
  * Tested up to: 4.7.5
  *
- * Text Domain: wc-steem
+ * Text Domain: wc-golos
  */
 
 // Exit if accessed directly
 if ( ! defined( 'ABSPATH' ) ) exit;
 
-define('WC_STEEM_VERSION', '0.0.1');
-define('WC_STEEM_DIR_PATH', trailingslashit(plugin_dir_path(__FILE__)));
-define('WC_STEEM_DIR_URL', trailingslashit(plugin_dir_url(__FILE__)));
+define('WC_GOLOS_VERSION', '0.0.1');
+define('WC_GOLOS_DIR_PATH', trailingslashit(plugin_dir_path(__FILE__)));
+define('WC_GOLOS_DIR_URL', trailingslashit(plugin_dir_url(__FILE__)));
 
-register_activation_hook(__FILE__, 'wc_steem_activate');
-register_deactivation_hook(__FILE__, 'wc_steem_deactivate');
+register_activation_hook(__FILE__, 'wc_golos_activate');
+register_deactivation_hook(__FILE__, 'wc_golos_deactivate');
 
 /** 
  * Plugin activation
  *
  * @since 1.0.0
  */
-function wc_steem_activate() {
-	do_action('wc_steem_activated');
+function wc_golos_activate() {
+	do_action('wc_golos_activated');
 
-	$settings = get_option('woocommerce_wc_steem_settings', array());
+	$settings = get_option('woocommerce_wc_golos_settings', array());
 
 	if ( ! isset($settings['accepted_currencies'])) {
 		$settings['accepted_currencies'] = array(
@@ -42,7 +42,7 @@ function wc_steem_activate() {
 	update_option('woocommerce_wc_steem_settings', $settings);
 
 	// Make sure to have fresh currency rates
-	update_option('wc_steem_rates', array());
+	update_option('wc_golos_rates', array());
 }
 
 /**
@@ -50,11 +50,11 @@ function wc_steem_activate() {
  *
  * @since 1.0.0
  */
-function wc_steem_deactivate() {
-	do_action('wc_steem_deactivated');
+function wc_golos_deactivate() {
+	do_action('wc_golos_deactivated');
 
 	// Make sure to have fresh currency rates
-	update_option('wc_steem_rates', array());
+	update_option('wc_golos_rates', array());
 }
 
 /**
@@ -62,14 +62,14 @@ function wc_steem_deactivate() {
  * 
  * @since 1.0.0
  */
-function wc_steem_init() {
+function wc_golos_init() {
 
 	/**
 	 * Fires before including the files
 	 *
 	 * @since 1.0.0
 	 */
-	do_action('wc_steem_pre_init');
+	do_action('wc_golos_pre_init');
 
 	require_once(WC_STEEM_DIR_PATH . 'libraries/wordpress.php');
 	require_once(WC_STEEM_DIR_PATH . 'libraries/woocommerce.php');
@@ -91,23 +91,23 @@ function wc_steem_init() {
 	 *
 	 * @since 1.0.0
 	 */
-	do_action('wc_steem_init');
+	do_action('wc_golos_init');
 }
-add_action('plugins_loaded', 'wc_steem_init');
+add_action('plugins_loaded', 'wc_golos_init');
 
 
 
 /**
- * Register "WooCommerce Steem" as payment gateway in WooCommerce
+ * Register "WooCommerce Golos" as payment gateway in WooCommerce
  *
  * @since 1.0.0
  *
  * @param array $gateways
  * @return array $gateways
  */
-function wc_steem_register_gateway($gateways) {
-	$gateways[] = 'WC_Gateway_Steem';
+function wc_golos_register_gateway($gateways) {
+	$gateways[] = 'WC_Gateway_Golos';
 
 	return $gateways;
 }
-add_filter('woocommerce_payment_gateways', 'wc_steem_register_gateway');
+add_filter('woocommerce_payment_gateways', 'wc_golos_register_gateway');
